3f5246cd54a7961d173d3655dc96060733bd01ab
pcfu <pfu@sephora.sg>
2024-03-23 11:49:55 +0800

    initial commit

---
+++ src/main/java/com/sephora/fuel/springminimal/SpringMinimalApplication.java
@@ -0,0 +1,13 @@
+package com.sephora.fuel.springminimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMinimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMinimalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sephora/fuel/springminimal/SpringMinimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sephora.fuel.springminimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMinimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
