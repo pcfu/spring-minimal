391ffe656fed63f8ca58d6e3cde8be1b513530a5
pcfu <pfu@sephora.sg>
2024-03-26 09:35:29 +0800

    enchance run-server script to kill server on interrupt

--- dockerdev/run-server.sh
+++ dockerdev/run-server.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
 
+oninterrupt() {
+    trap - SIGINT
+
+    pid=$(lsof -t -i:8080)
+    if [[ -n "$pid" ]]; then
+        kill -9 $pid
+    fi
+
+    exit
+}
+
+
 ./gradlew bootRun &
+
+trap oninterrupt SIGINT
 
 inotifywait -m -e modify,create,delete,move -r ./src/ | while read dir events basename; do
     echo "[inotifywait] $dir$basename - $events"
